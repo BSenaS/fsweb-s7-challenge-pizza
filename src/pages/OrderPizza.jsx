d303d25583072de6165ef0c8a8ca8d504fd9b0a0
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-01-09 00:01:53 +0300

    ufak css değişiklikleri (%30)

--- src/pages/OrderPizza.jsx
+++ src/pages/OrderPizza.jsx
@@ -21,43 +21,52 @@ export default function OrderPizza(){
   return(
     <>
     <Header/>
+    
     <div className="order-section">
-      <h2>Position Absolute Acı Pizza</h2>
+      <h2 className="pizza-name">Position Absolute Acı Pizza</h2>
       <div className="order-fiyat">
-        <p>85.50₺</p>
+        <p className="pizza-fiyati-p">85.50₺</p>
         <div className="order-fiyat-stock">
           <p>4.9</p>
           <p>(200)</p>
         </div>
       </div>
-      <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
+      <p className="ürün-aciklama">Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
 
       <div className="order-options">
         <div className="order-options-size">
-        <h3>Boyut Seç *</h3>
-        <input type="radio" id="kucuk"/>
-        <label htmlFor="kucuk">Küçük</label>
+          <h3 className="h3-aciklamalar">Boyut Seç *</h3>
 
-        <input type="radio" id="orta"/>
-        <label htmlFor="kucuk">Orta</label>
+          <div>
+          <input type="radio" id="kucuk" />
+          <label htmlFor="kucuk" className="boyut-sec">Küçük</label>
+          </div>
+          
+          <div>
+          <input type="radio" id="orta"/>
+          <label htmlFor="kucuk" className="boyut-sec">Orta</label>
+          </div>
 
-        <input type="radio" id="buyuk"/>
-        <label htmlFor="buyuk">Büyük</label>
+          <div>
+          <input type="radio" id="buyuk"/>
+          <label htmlFor="buyuk" className="boyut-sec">Büyük</label>
+          </div>
         </div>
 
         <div className="order-options-type">
-          <h3>Hamur Seç *</h3>
-          <select name="cars">
-            <option value="ince">İnce</option>
-            <option value="orta">Orta</option>
-            <option value="kalin">Kalın</option>
+          <h3 className="h3-aciklamalar">Hamur Seç *</h3>
+          <select name="options">
+            <option value="ince">Hamur Kalınlığı</option>
+            <option value="ince" className="boyut-sec">İnce</option>
+            <option value="orta" className="boyut-sec">Orta</option>
+            <option value="kalin" className="boyut-sec">Kalın</option>
           </select>
         </div>
       </div>
 
       <div className="malzeme-container">
         <div className="malzeme-paragraph">
-          <h3>Ek Malzemeler</h3>
+          <h3 className="h3-aciklamalar">Ek Malzemeler</h3>
           <p>En Fazla 10 malzeme seçebilirsiniz. 5₺</p>
         </div>
         {/* CheckboxListe map atarak, checkboxlar oluşturma */}
