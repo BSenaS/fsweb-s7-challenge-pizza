87cd2ba4ee59985abde91806357cb62e5eef1e48
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-01-10 13:23:59 +0300

    Success Page , html css + Routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import "./home.css";
+import "../mvp-assets/mvp-banner.png";
+import "../mvp-assets/logo.svg";
+import { useHistory } from 'react-router-dom';
+
 
 export default function Home() {
+  const history = useHistory();
+
+  const handleButtonClick = () => {
+    // Yönlendirme işlemi
+    history.push('/pizza');
+  };
   return (
-    <div>
-      <p>Burası Home Page Olacak</p>
-    </div>
+      <div className='home-banner'>
+        <div className='home-wrapper' >
+          <h1>Teknolojik Yemekler</h1>
+          <div className='h2-wrapper'>
+            <h2>KOD ACIKTIRIR PİZZA, DOYURUR</h2>
+          </div>
+          <button onClick={handleButtonClick}>ACIKTIM</button>
+        </div>
+      </div>
     
   );
 }
