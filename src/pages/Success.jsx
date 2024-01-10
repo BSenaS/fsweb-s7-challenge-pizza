d45d83555c8e0453fa5bfb6b2609a19fcc9f1170
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-01-10 13:40:15 +0300

    Success Page Html / Css

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./success.css";
 
 export default function Success() {
   return (
-    <div>
-      <p>Burası Success Page Olacak</p>
+    <div className='main-wrapper'>
+      <h1>Teknolojik Yemekler</h1>
+      <div className='siparis-wrapper'>
+        <p>TEBRİKLER! SİPARİŞİNİZ ALINDI!</p>
+      </div>
     </div>
     
   );
